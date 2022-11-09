e8f30a2ba6ea9b31f6f0fb6ec713ffa3dcb758ac
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-11-09 21:45:14 +0330

    FIX: Plural names.

--- university/admin.py
+++ university/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from university.models import Country, City, University
 
-# Register your models here.
+# Register your models here
+
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(University)
--- university/models.py
+++ university/models.py
@@ -11,6 +11,9 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Countries'
+
 
 class City(models.Model):
     name = models.CharField(max_length=50, verbose_name='City name')
@@ -19,12 +22,15 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Cities'
+
 
 class University(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='universities', verbose_name='City')
     name = models.CharField(max_length=100, verbose_name='University name')
     acronym = models.CharField(max_length=10, verbose_name='University acronym')
-    logo = models.ImageField(upload_to='images/univercity_logos', verbose_name='University logo')
+    logo = models.ImageField(upload_to='images/university_logos', verbose_name='University logo')
     type = models.CharField(max_length=100, verbose_name='University type')
     overview = models.TextField(verbose_name='University overview')
     since = models.IntegerField(validators=[MaxValueValidator(2022)], verbose_name='University since')
@@ -35,3 +41,6 @@ class University(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'Universities'
